ff9ce86898182d72268baa3c77000fc1f18afa46
주효식 <lux1937@gmail.com>
2021-06-30 14:48:56 +0000

    Video 2.1 : Install : Node.js

---
+++ main.js
@@ -0,0 +1,10 @@
+import { join } from 'path'
+import { Low, JSONFile } from 'lowdb'
+
+// Use JSON file for storage
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+// Read data from JSON file, this will set db.data content
+await db.read()
